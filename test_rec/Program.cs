cca2d9ca43893377505b694241d3ed9bd6219008
OmidShams <omidrazzaghi2000@gmail.com>
2024-06-29 17:31:42 +0330

    [ADD]: Add receiver thread

--- test_rec/Program.cs
+++ test_rec/Program.cs
@@ -54,13 +54,15 @@ namespace test_rec
             //Generate a random packet
             byte[] bytes = GetRandomPacket();
 
+            int pktCounter = 0;
+
             while (true)
             {
                 try
                 {
                     //Send the packet out the network device
                     device.SendPacket(bytes);
-                    Console.WriteLine("-- Packet sent successfuly.");
+                    //Console.WriteLine("-- Packet {0} sent successfuly.",pktCounter++);
                 }
                 catch (Exception e)
                 {
@@ -77,6 +79,17 @@ namespace test_rec
             send_thread_object.Start();
 
 
+            void Device_OnPacketArrival(object s, PacketCapture e)
+            {
+                Console.WriteLine(BitConverter.ToString(e.Data.ToArray()));
+            }
+
+            using var device = LibPcapLiveDeviceList.Instance[5];
+            device.Open();
+            device.OnPacketArrival += Device_OnPacketArrival;
+            device.StartCapture();
+
+
             Console.Write("Hit 'Enter' to exit...");
             Console.ReadLine();
         }
@@ -88,9 +101,15 @@ namespace test_rec
         /// </summary>
         private static byte[] GetRandomPacket()
         {
-            byte[] packet = new byte[6232];
+            byte[] packet = new byte[60];
             Random rand = new Random();
             rand.NextBytes(packet);
+            packet[0] = 0xaa;
+            packet[1] = 0xbb;
+            packet[2] = 0xcc;
+            packet[3] = 0xdd;
+            packet[4] = 0xee;
+            packet[5] = 0xff;
             return packet;
         }
     }
